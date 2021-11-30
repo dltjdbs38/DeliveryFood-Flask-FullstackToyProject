8d562c98fa5f65a213754aa80dfc3acd11c5ac1d
Seoyoon-Scalar <aassdd38@naver.com>
2021-11-30 23:15:01 +0900

    render_template('index.html')

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask,jsonify
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/') # 서버에 접속할 수 있는 url을 만들어 준다.
 def greeting(): # app.route()의 url에서 실행할 함수
-    my_data = {'name':'seoyoon'}
-    return jsonify(my_data)
+    # 1개의 app.route는 1개의 함수와 연결될 수 있다.
+    return render_template('index.html')
 
 
 if __name__=='__main__': # 파일 이름이 main일때만 app.run()이 실행되도록 한다.
