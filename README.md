6f6f446d7f8f87ce5ebc0f9a85fbc40673c431dd
Seoyoon-Scalar <aassdd38@naver.com>
2021-12-01 01:52:47 +0900

    request.args.get('name') /뒤에 ?name=seoyoon 치면 웹페이지에 "seoyoon"이라고 출력됨

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 
 1. python3 -m pip install flask
 2. 플라스크 실행 python3 app.py(=실행할 파일)
+
+REST API란?
+
+- HTTP URI를 통해 자원을 명시하고, HTTP Method(POST, GET, PUT, DELETE)를 통해
+  해당 자원에 대한 CRUD Operation을 적용하는 것
+
+- GET = 조회, POST = 생성/수정/삭제
+
+GET
+
+- 데이터를 URL뒤에 ?와 함꼐 사용
+
+POST
+
+- 특정 양식에 데이터를 넣어 전송
--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask,jsonify,request
 app = Flask(__name__)
 
-@app.route('/') # 서버에 접속할 수 있는 url을 만들어 준다.
+@app.route('/',methods=['GET']) # 서버에 접속할 수 있는 url을 만들어 준다.
 def greeting(): # app.route()의 url에서 실행할 함수
     # 1개의 app.route는 1개의 함수와 연결될 수 있다.
-    return render_template('index.html')
+    name = request.args.get('name') # http://127.0.0.1:5000/?name=seoyoon
+    return jsonify(name) # 웹페이지에 "seoyoon"이라고 뜸.
 
 
 if __name__=='__main__': # 파일 이름이 main일때만 app.run()이 실행되도록 한다.
