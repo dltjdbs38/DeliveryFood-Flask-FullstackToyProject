78183d281ac5785e87070cd313523ce2e84a865c
Seoyoon-Scalar <aassdd38@naver.com>
2021-11-30 23:12:18 +0900

    jsonify와 README 추가, templates에 index.html 추가

---
+++ README.md
@@ -0,0 +1,4 @@
+사용법
+
+1. python3 -m pip install flask
+2. 플라스크 실행 python3 app.py(=실행할 파일)
--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask,jsonify
 app = Flask(__name__)
 
 @app.route('/') # 서버에 접속할 수 있는 url을 만들어 준다.
 def greeting(): # app.route()의 url에서 실행할 함수
-    return 'hello world'
+    my_data = {'name':'seoyoon'}
+    return jsonify(my_data)
+
 
 if __name__=='__main__': # 파일 이름이 main일때만 app.run()이 실행되도록 한다.
     app.run()
