df330d5830ecd6e1162ff2eadc8c473729b375b1
Seoyoon-Scalar <aassdd38@naver.com>
2021-12-09 23:43:13 +0900

    500에러 수정 및 login 추가 (그러나 board기능이나 로그인 기능이 완전히 구현된 것 X)

--- app.py
+++ app.py
@@ -39,19 +39,31 @@ def register_service():
 
     user = {'name':id, 'password':pw}
     user_list.append(user)
-    return redirect('/')
+    return redirect('/') 
 
-@app.route('/board', methods=['POST'])
+@app.route('/board', methods=['POST']) # 추가할 글의 제목, 내용을 board_list에 추가
 def writer_service():
     name = request.form['username']
     content = request.form['content']
     data = {'name':name, 'content':content}
     board_list.append(data)
+    return redirect('/board') # 아직 제대로 완성된 기능이 아님.
+
+@app.route('/board/<id>') # 추가된 글의 내용 수정, id는 작성자의 아이디가 아닌 글의 아이디
+def edit_board(id): 
+    content = request.form['content']
+    board_list[id+1]['content']=content # 해당 글의 내용에 접근 후 수정
     return redirect('/board')
 
-@app.route('/board/<id>')
-def edit_board(id)
-
-
+@app.route('/login',methods=['POST']) # <form action="/login" method='post'></form>
+# <form>의 action 속성: 서식 데이터(form data)를 서버로 보낼 때 해당 데이터가 도착할 URL
+# <form>의 method 속성: 폼 데이터(form data)가 서버로 제출될 때 사용되는 HTTP 메소드를 명시
+def login_service():
+    id = request.form['username']
+    pw = request.form['password']
+    for user in user_list:
+        if user['name'] ==id and user['password']==pw: # 입력한 id와 pw가 회원목록에 있으면
+            return jsonify(f'로그인 성공! 안녕하세요 {id}님')
+    return jsonify('로그인 실패! 회원정보를 다시 확인해주시거나 회원가입을 해주세요.')
 if __name__=='__main__': # 파일 이름이 main일때만 app.run()이 실행되도록 한다.
     app.run()
